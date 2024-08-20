74d8139ac79a16c8bf9ab50f4565eebff2b5e4cc
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-20 15:19:33 +0300

    Add files via upload

---
+++ odev5.sql
@@ -0,0 +1,19 @@
+/* 1. Film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sýralama*/
+SELECT title, length
+FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+/* 2. Film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kýsa (length) ikinci(6,7,8,9,10) 5 filmi (6,7,8,9,10) sýralama*/
+SELECT title, length
+FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+/* 3. Customer tablosunda bulunan last_name sütununa göre azalan yapýlan sýralamada store_id 1 olmak koþuluyla ilk 4 veriyi sýralama*/
+SELECT first_name, last_name, store_id
+FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
