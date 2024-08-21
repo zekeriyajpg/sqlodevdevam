abac79d3c97c2ac4da69665181ca65f20ba905cc
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-21 09:54:25 +0300

    Add files via upload

---
+++ odev9.sql
@@ -0,0 +1,15 @@
+--1. city ve country tablolarýný birleþtirme--
+SELECT city.city, country.country 
+FROM city 
+INNER JOIN country 
+ON city.country_id = country.country_id;
+--2. customer ve payment tablolarýný birleþtirme--
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id;
+--3. customer ve rental tablolarýný birleþtirme--
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+INNER JOIN customer
+ON rental.customer_id = customer.customer_id;
